e477fe9c026e928007bb32dac56bf8b7eb2a99f5
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-11-15 18:20:45 -0300

    atualizações

--- Shoes-Shop/src/Pages/Home/index.tsx
+++ Shoes-Shop/src/Pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import { useCart } from '../../hook/useCart'
+// import { useCart } from '../../hook/useCart'
 import { HomeContainer, Shoes } from '../../styles/pages/Home'
 import { Header } from './components/Header'
 
-export interface Product {
+/* export interface Product {
   id: number
   title: string
   price: number
@@ -10,37 +10,28 @@ export interface Product {
   quantity: number
 }
 
-interface ShoesProps {
+interface ProductProps {
   product: Product
-}
+} */
 
-export const Home = ({ product }: ShoesProps) => {
-  const { addToCart } = useCart()
+export const Home = (/* { product }: ProductProps */) => {
+  /* const { addToCart } = useCart()
   const handleAddToCart = () => {
     const shoesToAdd = {
       ...product,
     }
     addToCart(shoesToAdd)
-  }
+  } */
   return (
     <HomeContainer>
       <Header />
       <Shoes>
-        <div key={product.id}>
+        {/* <div key={product.id}>
           <img src={product.image} alt="" />
-          <h2>{product.title}</h2>
           <span>{product.price}</span>
+          <h2>{product.title}</h2>
           <button onClick={handleAddToCart}>ADICIONAR AO CARRINHO</button>
-        </div>
-
-        {/* {Products.map((product) => (
-          <div key={product.id}>
-            <img src={product.image} alt="" />
-            <h2>{product.title}</h2>
-            <span>{product.price}</span>
-            <button onClick={handleAddToCart}>ADICIONAR AO CARRINHO</button>
-          </div>
-        ))} */}
+        </div> */}
       </Shoes>
     </HomeContainer>
   )
--- Shoes-Shop/src/data/ProductData.tsx
+++ Shoes-Shop/src/data/ProductData.tsx
@@ -1,49 +1,65 @@
 import PhotoShoes from '../assets/tenis.webp'
-import { Home } from '../Pages/Home'
+
+// import { Home } from '../Pages/Home'
+import { DataContainer } from './ProductDta'
+
 export const PhotoShoess = () => {
   const Products = [
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 240.9,
+      id: 1,
+      title: 'Expresso Tradicional',
       image: PhotoShoes,
+      price: 9.9,
+      quantity: 1,
     },
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 140.0,
+      id: 2,
       image: PhotoShoes,
+      title: 'Expresso Americano',
+      price: 9.9,
+      quantity: 1,
     },
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 200,
+      id: 3,
       image: PhotoShoes,
+      title: 'Expresso Cremoso',
+      price: 9.9,
+      quantity: 1,
     },
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 99.9,
+      id: 4,
       image: PhotoShoes,
+      title: 'Expresso Gelado',
+      price: 9.9,
+      quantity: 1,
     },
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 120,
+      id: 5,
       image: PhotoShoes,
+      title: 'Café com Leite',
+      price: 9.9,
+      quantity: 1,
     },
     {
-      id: crypto.randomUUID(),
-      title: 'Tenis de corrida super confortavel',
-      price: 350.5,
+      id: 6,
       image: PhotoShoes,
+      title: 'Latte',
+      price: 9.9,
+      quantity: 1,
     },
   ]
+
   return (
-    <div>
-      {Products.map((product) => (
-        <Home key={product.id} product={product} />
-      ))}
-    </div>
+    <DataContainer>
+      <h1>Meus Tenis</h1>
+
+      <div>
+        {Products.map((product) => {
+          console.log(product)
+          return <h1 key={product.id}>ola</h1>
+          /* <Home key={product.id} product={product} /> */
+        })}
+      </div>
+    </DataContainer>
   )
 }
